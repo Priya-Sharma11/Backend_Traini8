8c577ffdb31fc9b1e096cee630d48bd34df89265
Priya Sharma <83345365+Priya-Sharma11@users.noreply.github.com>
2024-10-07 17:10:20 +0530

    Update TrainingCenterService.java

--- project/src/main/java/com/traini8/project/service/TrainingCenterService.java
+++ project/src/main/java/com/traini8/project/service/TrainingCenterService.java
@@ -7,21 +7,30 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+/**
+ * Service class for managing TrainingCenter entities.
+ * Provides business logic for CRUD operations and data retrieval.
+ */
+
 @Service
 public class TrainingCenterService {
 
     @Autowired
     private TrainingCenterRepository trainingCenterRepository;
 
+
+     //Saves a new TrainingCenter entity to the database.
     public TrainingCenter savedTrainingCenter(TrainingCenter trainingCenter){
         trainingCenter.setCreatedOn(System.currentTimeMillis()/1000L);
         return trainingCenterRepository.save(trainingCenter);
     }
-
+    
+    //Retrieves all TrainingCenter entities from the database.
     public List<TrainingCenter> getAllTrainingCenters(){
         return trainingCenterRepository.findAll();
     }
 
+    //Retrieves TrainingCenter entities based on optional filters for city, state, and course.
     public List<TrainingCenter> getTrainingCentersByFilter(String city, String state, String course){
         return trainingCenterRepository.findByFilters(city,state,course);
     }
