7686b89d87f7aa5184c0cd3a472786b83d798920
Priya Sharma <83345365+Priya-Sharma11@users.noreply.github.com>
2024-10-07 17:08:38 +0530

    Update TrainingCenterRepository.java

--- project/src/main/java/com/traini8/project/repository/TrainingCenterRepository.java
+++ project/src/main/java/com/traini8/project/repository/TrainingCenterRepository.java
@@ -8,24 +8,46 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+/**
+ * Repository interface for managing TrainingCenter entities.
+ * Extends JpaRepository for basic CRUD operations and 
+ * JpaSpecificationExecutor for advanced queries using specifications.
+ */
+
+
 @Repository
 public interface TrainingCenterRepository extends JpaRepository<TrainingCenter,Long> , JpaSpecificationExecutor<TrainingCenter> {
 
+     /**
+     * Finds TrainingCenter entities based on optional filters for city, state, and course.
+     * 
+     * @param city the city to filter by (optional)
+     * @param state the state to filter by (optional)
+     * @param course the course to filter by (optional)
+     * @return a list of TrainingCenter entities matching the specified filters
+     */
+    
     default List<TrainingCenter> findByFilters(String city, String state, String course) {
         Specification<TrainingCenter> spec = Specification.where(hasCity(city))
                 .and(hasState(state))
                 .and(hasCourse(course));
-        return findAll(spec);
+        return findAll(spec);                        // Execute the specification and return the results
     }
 
+     
+     //Specification to filter TrainingCenters by city.
+     
     private static Specification<TrainingCenter> hasCity(String city){
         return (root,query, criteriaBuilder)->city==null ? null :criteriaBuilder.equal(root.get("address").get("city"), city);
     }
 
+    
+     //Specification to filter TrainingCenters by state.
     private static Specification<TrainingCenter> hasState(String state){
         return (root,query,criteriaBuilder)-> state==null ? null : criteriaBuilder.equal(root.get("address").get("state"),state);
     }
 
+     //Specification to filter TrainingCenters by course.
     private static Specification<TrainingCenter> hasCourse(String course){
         return (root,query,criteriaBuilder)-> course==null ? null : criteriaBuilder.isMember(course, root.get("coursesOffered"));
     }
